493747533acbbd64f52d8eb69cc68f38f89d1513
Averkiev91 <amd3500m@gmail.com>
2022-10-12 13:55:42 +0300

    head section

--- src/script/script.js
+++ src/script/script.js
@@ -1,2 +1,19 @@
 import '../index.html'
 import '../style/style.css' // Импортируем стили css
+// import Swiper JS
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+ const swiper = new Swiper('.top-slider', {
+     modules: [Navigation, Pagination],
+     pagination: {
+         el: '.swiper-pagination',
+     },
+     navigation: {
+         nextEl: '.top-slider__button-next',
+         prevEl: '.top-slider__button-prev',
+     }
+ });
